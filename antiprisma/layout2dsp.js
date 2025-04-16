a23e5904394b8d477c69c7a05c6de56d7b90cfd2
Basil Stotz <stotz.basil@opinsys.fi>
2025-04-16 11:54:33 +0200

    add missing cos(e)

--- antiprisma/layout2dsp.js
+++ antiprisma/layout2dsp.js
@@ -9,20 +9,19 @@ function layoutToDsp(layout){
     //console.log(layout);
     let speakers = layout.LoudspeakerLayout.Loudspeakers;
     //console.log(speakers);
-    let txt='';
+    let txt = '';
     let N = speakers.length; 
     txt+='N = '+N+';\n';
     for(let i=0;i<N;i++){
-	let spk=speakers[i];
-	let x,y;
-	if(Math.abs(spk.Elevation)>=88){
-	    x=0;
-	    y=0;
-	}else{
-	    x=Math.cos(spk.Azimuth*(Math.PI/180))*spk.Radius;
-	    y=Math.sin(spk.Azimuth*(Math.PI/180))*spk.Radius;
-	}
-	let z=Math.sin(spk.Elevation*(Math.PI/180))*spk.Radius;
+	let spk = speakers[i];
+	
+	let a = spk.Azimuth*(Math.PI/180);
+	let e = spk.Elevation*(Math.PI/180);
+	let r = spk.Radius;
+	
+	let x = Math.cos(a)*Math.cos(e)*r;
+	let y = Math.sin(a)*Math.cos(e)*r;
+	let z = Math.sin(e)            *r;
 	txt+='speaker('+i+') = ( '+round(x)+', '+round(y)+', '+round(z)+' );\n';
     }
     return txt
